ea12be8e5191ea61daf1e27e574030ac633ef528
Anna Soldatova <anna.glebova1995@yandex.ru>
2025-05-20 18:07:42 +0400

    "Issue 14 Для вещи добавить поле стоимость вещи за день"

--- entity/thing.go
+++ entity/thing.go
@@ -2,14 +2,17 @@ package entity
 
 import (
 	"main/types"
+	"time"
 )
 
 type Thing struct {
 	ID        int              `json:"id"`
 	Name      string           `json:"name"`
-	PayDate   string           `json:"pay_date"`
+	PayDate   time.Time        `json:"pay_date"`
 	PayPrice  int              `json:"pay_price"`
 	SaleDate  types.NullString `json:"sale_date"`
 	SalePrice types.NullInt64  `json:"sale_price"`
+	Days      int              `json:"days"`
+	PayDay    float64          `json:"pay_day"`
 	CreatedAt string           `json:"-"`
 }
