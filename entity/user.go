f0492f6c7775637d49b99a33d4e653cbe7dc79cc
Anna Soldatova <anna.glebova1995@yandex.ru>
2025-06-03 15:24:35 +0400

    Issue 26 Добавление новой таблицы "пользователи

---
+++ entity/user.go
@@ -0,0 +1,14 @@
+package entity
+
+import "time"
+
+type User struct {
+	ID        int       `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	IsDeleted bool      `json:"is_deleted"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
