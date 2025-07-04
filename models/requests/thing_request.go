e39cdaf6ba591b92984fae09f1139a85ab3fed78
Anna Soldatova <anna.glebova1995@yandex.ru>
2025-07-04 15:59:04 +0400

    "Issue 46 Создать requests для thing и expense и валидацию для контроллеров

---
+++ models/requests/thing_request.go
@@ -0,0 +1,14 @@
+package requests
+
+import (
+	"main/types"
+	"time"
+)
+
+type ThingRequest struct {
+	Name      string          `json:"name" validate:"required,min=3"`
+	PayDate   time.Time       `json:"pay_date" validate:"required"`
+	PayPrice  int             `json:"pay_price" validate:"required,gt=0"`
+	SaleDate  types.NullTime  `json:"sale_date"`
+	SalePrice types.NullInt64 `json:"sale_price"`
+}
