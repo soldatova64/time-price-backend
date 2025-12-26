34cbbee3b8dbe5083da9360cd28aac115b2fe8d7
Anna Soldatova <anna.glebova1995@yandex.ru>
2025-12-26 14:43:15 +0400

    Issue 72 При редактировании пользователя убрать редактирование username

--- models/requests/user_update_request.go
+++ models/requests/user_update_request.go
@@ -1,6 +1,5 @@
 package requests
 
 type UserUpdateRequest struct {
-	Username *string `json:"username,omitempty" validate:"omitempty,min=3"`
 	Password *string `json:"password,omitempty" validate:"omitempty,min=8,containsany=ABCDEFGHIJKLMNOPQRSTUVWXYZ,containsany=abcdefghijklmnopqrstuvwxyz,containsany=0123456789"`
 }
